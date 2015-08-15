dc6c0e770332bb7fc2b640764ee51c29234e8754
Sergey Odinokov <odinserj@gmail.com>
2015-08-15 16:14:13 +0300

    Initial commit

---
+++ Samples/States.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hangfire;
+using Hangfire.States;
+
+namespace Samples
+{
+    public class States
+    {
+        public void CreateInEnqueuedState()
+        {
+            #region EnqueuedState #1
+            var client = new BackgroundJobClient();
+            var state = new EnqueuedState("critical"); // Use the "critical" queue
+             
+            client.Create(() => Console.WriteLine("Hello!"), state);
+            #endregion
+        }
+
+        public void ChangeToEnqueuedState()
+        {
+            var jobId = "someid";
+
+            #region EnqueuedState #2
+            var client = new BackgroundJobClient();
+            var state = new EnqueuedState(); // Use the default queue
+            
+            client.ChangeState(jobId, state, FailedState.StateName);
+            #endregion
+        }
+    }
+}
---
+++ VBSamples/States.vb
@@ -0,0 +1,24 @@
+﻿Imports Hangfire
+Imports Hangfire.States
+
+Public Class States
+    Sub CreateInEnqueuedState
+        #Region "EnqueuedState #1"
+        Dim client = new BackgroundJobClient()
+        Dim state = new EnqueuedState("critical") ' Use the "critical" queue
+             
+        client.Create(Sub () Console.WriteLine("Hello"), state)
+        #End Region
+    End Sub
+
+    Sub ChangeToEnqueuedState
+        Dim jobId = "someid"
+
+        #Region "EnqueuedState #2"
+        Dim client = new BackgroundJobClient()
+        Dim state = new EnqueuedState() ' Use the default queue
+            
+        client.ChangeState(jobId, state, FailedState.StateName)
+        #End Region
+    End Sub
+End Class
