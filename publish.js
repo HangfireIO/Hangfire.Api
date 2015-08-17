0ede3a8d24b63476b0f11dc22f1cbc61123b4f00
Sergey Odinokov <odinserj@gmail.com>
2015-08-17 17:36:28 +0300

    Update publish script to msbuild the project before publishing

--- publish.js
+++ publish.js
@@ -1,9 +1,21 @@
 var ghpages = require('gh-pages');
 var path = require('path');
+var msbuild = require('msbuild');
 
-ghpages.publish(path.join(__dirname, 'Hangfire.Api/help'), {
-  logger: function(message) {
-    console.log(message);
-  },
-  add: true,
-});
+var publish = function () {
+  console.log("Publishing the project...");
+  ghpages.publish(path.join(__dirname, 'Hangfire.Api/help'), {
+    logger: function(message) {
+      console.log(message);
+    },
+    add: true,
+  });
+};
+
+var build = new msbuild(publish); 
+build.sourcePath = path.join(__dirname, 'Hangfire.Api.sln');
+build.configuration = 'debug';
+build.publishProfile='';
+
+console.log("Building the project...");
+build.build();
