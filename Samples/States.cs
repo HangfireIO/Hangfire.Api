e8a815c4290ddc20cf4f18901da50ba1e95907b3
Sergey Odinokov <odinserj@gmail.com>
2015-08-17 17:36:40 +0300

    Add some more samples for states

--- Samples/States.cs
+++ Samples/States.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Hangfire;
 using Hangfire.States;
 
@@ -27,5 +28,49 @@ namespace Samples
             client.ChangeState(jobId, state, FailedState.StateName);
             #endregion
         }
+
+        public void ChangeToDeletedState()
+        {
+            #region DeletedState
+            var client = new BackgroundJobClient();
+            var jobId = client.Enqueue(() => Console.WriteLine("Hello"));
+
+            var state = new DeletedState();
+            client.ChangeState(jobId, state, EnqueuedState.StateName);
+            #endregion
+        }
+
+        #region FaultedState
+        public class FaultedState : IState
+        {
+            public static readonly string StateName = "Faulted";
+
+            public FaultedState(Exception exception)
+            {
+                Message = exception.Message;
+                Details = exception.ToString();
+            }
+
+            public string Name => StateName;
+            public string Reason { get; set; }
+            
+            public bool IsFinal => true;
+            public bool IgnoreJobLoadException => true;
+
+            // Here are our custom properties, we'll made them
+            // accessible through the Dashboard UI.
+            public string Message { get; }
+            public string Details { get; }
+
+            public Dictionary<string, string> SerializeData()
+            {
+                return new Dictionary<string, string>
+                {
+                    { "Message", Message },
+                    { "Details", Details }
+                };
+            }
+        }
+        #endregion
     }
 }
