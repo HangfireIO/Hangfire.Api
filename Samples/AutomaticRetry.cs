4e3ba7cd7b69c2475d4cf9f31f9ec43e48094f66
Sergey Odinokov <odinserj@gmail.com>
2015-09-08 17:26:03 +0300

    Add some examples for the AutomaticRetryAttribute

---
+++ Samples/AutomaticRetry.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+
+namespace Samples
+{
+    class AutomaticRetry
+    {
+        #region Disable Retries
+        [AutomaticRetry(Attempts = 0)]
+        public void RetriesDisabled(string arg)
+        {
+        }
+        #endregion
+
+        public void OverrideDefault()
+        {
+            #region Override Default
+            // Call it in the application initialization logic
+            GlobalJobFilters.Filters.Add(new AutomaticRetryAttribute { Attempts = 3 });
+            #endregion
+        }
+
+        #region Attempts Exceeded
+        [AutomaticRetry(OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+        public void DeleteWhenAttemptsExdeeded()
+        {
+        }
+        #endregion
+    }
+}
